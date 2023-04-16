7f28d42061186ca3424f9edcca197797360df54a
Cristinasj <cristinasj@protonmail.com>
2023-04-16 13:38:44 +0200

    GitHub

--- GVGAI-master/src/tracks/singlePlayer/evaluacion/src_SANCHEZ_JUSTICIA_CRISTINA/AgenteAStar.java
+++ GVGAI-master/src/tracks/singlePlayer/evaluacion/src_SANCHEZ_JUSTICIA_CRISTINA/AgenteAStar.java
@@ -1,5 +1,98 @@
 package tracks.singlePlayer.evaluacion.src_SANCHEZ_JUSTICIA_CRISTINA;
 
-public class AgenteAStar {
+import java.util.ArrayList;
+import java.util.PriorityQueue; 
+import core.game.StateObservation;
+import ontology.Types.ACTIONS;
+import tools.ElapsedCpuTimer;
 
+public class AgenteAStar extends Agente {
+	
+	ArrayList<ACTIONS> camino; 
+	ArrayList<ArrayList<Boolean>> esVisitable; 
+
+	public AgenteAStar(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		super(stateObs, elapsedTimer);
+		camino = new ArrayList<ACTIONS>(); 
+	}
+	
+	/**
+	 * @param stateObs: Observaciones del mundo 
+	 * @param inicio: Posición del inicio del avatar
+	 * @param coordenadasFin: Posicion del portal 
+	 * @return acciones que llevan al camino más corto
+	 */	
+	public ArrayList<ACTIONS> AStar (StateObservation stateObs,
+			ArrayList<Integer> inicio, 
+			ArrayList<Integer> coordenadasFin) {
+		Nodo fin = new Nodo(coordenadasFin.get(0), coordenadasFin.get(1), null, 
+				ACTIONS.ACTION_NIL);  
+		PriorityQueue<Nodo> abiertos = new PriorityQueue<Nodo>(); 
+		// forall nodo in grafo: g(nodo) = inf (automatico en clase Nodo)  
+		// visitados = []
+		ArrayList<ArrayList<Boolean>> esVisitable = llenarMatriz(stateObs);
+		// g(incial) = 0
+		Nodo nodoInicial = new Nodo(inicio.get(0), 
+				inicio.get(1), null, ACTIONS.ACTION_NIL);
+		nodoInicial.g = 0;
+		abiertos.add(nodoInicial); 
+		// while True: 
+		while (!abiertos.isEmpty()) {
+			// actual = argmin(g tal que not visitado)
+			Nodo actual = abiertos.poll(); 
+			// if actual == objetivo: 
+			if (actual.x == fin.x && actual.y == fin.y) {
+				// return, ya se ha encontrado el camino 
+				return actual.getCamino(); 
+			}
+			// visitados.add(actual) 
+			esVisitable.get(actual.x).set(actual.y, false); 
+			// foreach sucesor in expandir(actual):
+			for (Nodo sucesor : actual.generarSucesores()) {
+				// if not visitados.contains(sucesor) 
+				// and g(sucesor) > g(actual) 
+				// + distance(actual, sucesor):
+				if (esVisitable.get(sucesor.x).get(sucesor.y) 
+					&& sucesor.g > ( actual.g + actual.getDistancia(sucesor))) {
+					// g(sucesor) = g(actual) + 
+					// distancia(actual, sucesor) 
+					sucesor.g = actual.g + actual.getDistancia(sucesor); 
+					abiertos.add(sucesor);
+					sucesor.h = sucesor.getDistancia(fin); 
+				}
+				
+			}
+		
+		}
+		// El código solo llega aquí si no hay solución 
+		return new ArrayList<ACTIONS>(); 
+	}
+
+	@Override
+	public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		ArrayList<Integer> inicio = pasarAEntero(posicionAvatar(stateObs));
+		ACTIONS accion = null; 
+		// Si no hay camino calculado se calcula el camino 
+		if (camino.size() == 0) {
+			long tInicio = System.nanoTime(); 
+			camino = AStar(stateObs, inicio, portal);
+			System.out.println(camino); 
+			long tFin = System.nanoTime(); 
+			// Para rellenar la tabla
+			System.out.println("Runtime acumulado: " + (tFin - tInicio)/1000000);
+			System.out.println("Tamaño ruta: " + camino.size());
+			System.out.println("Numero de nodos: " + nodosExpandidos);
+			System.out.println("Máximo nº nodos en memoria: " + maxNodos); 	
+		}		
+		// Si ya tenemos el camino, se van sacando las posiciones 
+		if (camino.size() > 0) {
+			// pop
+			if (camino.size() > 0) {
+				accion = camino.get(0); 
+				camino.remove(0);		
+			}
+		} 
+		return accion;
+	}
 }
+
--- GVGAI-master/src/tracks/singlePlayer/evaluacion/src_SANCHEZ_JUSTICIA_CRISTINA/AgenteRTAStar.java
+++ GVGAI-master/src/tracks/singlePlayer/evaluacion/src_SANCHEZ_JUSTICIA_CRISTINA/AgenteRTAStar.java
@@ -1,5 +1,106 @@
 package tracks.singlePlayer.evaluacion.src_SANCHEZ_JUSTICIA_CRISTINA;
 
-public class AgenteRTAStar {
+import java.util.ArrayList; 
+import java.util.HashMap; 
+import java.util.PriorityQueue; 
+import core.game.StateObservation;
+import ontology.Types.ACTIONS;
+import tools.ElapsedCpuTimer;
 
+public class AgenteRTAStar extends Agente {
+	
+	ArrayList<ACTIONS> camino; 
+	int numLlamadasAct = 0; 
+	ArrayList<ArrayList<Boolean>> esVisitable;
+	boolean encontrado; // Salida de datos
+	HashMap<Integer, Integer> heuristicas;  
+
+	public AgenteRTAStar(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		super(stateObs, elapsedTimer);
+		camino = new ArrayList<ACTIONS>(); 
+		encontrado = false; 
+		heuristicas = new HashMap<>(); 
+	}
+	
+	/**
+	 * @param stateObs: Observaciones del mundo 
+	 * @param inicio: Posición del inicio del avatar
+	 * @param coordenadasFin: Posicion del portal 
+	 * @return acciones que llevan al camino más corto
+	 */	
+	public ACTIONS RTAStar (StateObservation stateObs,
+			ArrayList<Integer> inicio, 
+			ArrayList<Integer> coordenadasFin) {
+		Nodo nodoActual = new Nodo(inicio.get(0), 
+				inicio.get(1), null, ACTIONS.ACTION_NIL);
+		Nodo fin = new Nodo(coordenadasFin.get(0), coordenadasFin.get(1), null, 
+				ACTIONS.ACTION_NIL);  
+		Nodo nodoSiguiente = null; 
+		ArrayList<Nodo> vecindario = nodoActual.generarSucesores(); 
+		ArrayList<ArrayList<Boolean>> esVisitable = llenarMatriz(stateObs);
+		int f, heuristicaActual; 
+		final int INF = 99999; 
+		int mejorH = INF; 
+		int mejorH2 = INF; 
+		
+		for (Nodo vecino : vecindario) {
+			if (esVisitable.get(nodoActual.x).get(nodoActual.y)) {
+				// Se ha visitado el nodo 
+				if (heuristicas.containsKey(vecino.getID()))
+					f = heuristicas.get(vecino.getID());
+				else {
+					vecino.g = 0; 
+					vecino.h = vecino.getDistancia(fin); 
+					f = vecino.f(); 
+					
+				}
+				if (f < mejorH) {
+					mejorH2 = mejorH;
+					mejorH = f; 
+					nodoSiguiente = vecino; 
+				}
+				else 
+					if (f < mejorH2)
+						mejorH2 = f; 
+					
+				
+			}
+		}
+		if (heuristicas.containsKey(nodoActual.getID()))
+			heuristicaActual = heuristicas.get(nodoActual.getID()); 
+		else 
+			heuristicaActual = nodoActual.f(); 
+		// Se busca el máximo 
+		if (mejorH2 != INF && mejorH2 > heuristicaActual)
+			heuristicaActual = mejorH2 + 1; 
+		else {
+			if (mejorH+1 > heuristicaActual) {
+				heuristicaActual = mejorH+1; 
+			}
+		}
+		heuristicas.put(nodoActual.getID(), heuristicaActual); 
+		nodosExpandidos++; 
+		if (nodoSiguiente.esIgualA(fin))
+			encontrado = true; 
+		return nodoSiguiente.ultimaAccion; 
+	}
+	
+	@Override
+	public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+		numLlamadasAct++;
+		ArrayList<Integer> inicio = pasarAEntero(posicionAvatar(stateObs));
+		ACTIONS accion = null; 
+			long tInicio = System.nanoTime(); 
+			accion = RTAStar(stateObs, inicio, portal); 
+			long tFin = System.nanoTime(); 
+			// Para rellenar la tabla
+			if (encontrado) {
+				System.out.println("Runtime acumulado: " + (tFin - tInicio)/1000000);
+				System.out.println("Tamaño ruta: " + numLlamadasAct);
+				System.out.println("Numero de nodos: " + numLlamadasAct);
+				System.out.println("Máximo nº nodos en memoria: " + heuristicas.size()); 			
+			}
+		return accion;
+	}
 }
+
